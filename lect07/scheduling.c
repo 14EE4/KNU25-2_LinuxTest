b8f8caa422506967c902da25ed7a3e34777e66c3
14ee4 <pyeong85732@naver.com>
2025-11-19 00:24:28 +0900

    성능출력 탭문자추가

--- lect07/scheduling.c
+++ lect07/scheduling.c
@@ -114,7 +114,7 @@ void print_performance(){
 	printf("pno\trunning time\twait\tturnaround\n");
 	for (int i=0;i<PROCESS_NUM;i++){
 		PCB *pcb=&pcb_table[i];
-		printf("%d\t%d\t%d\t%d\n",
+		printf("%d\t%d\t\t%d\t%d\n",
 				i,
 				pcb->total_burst_time,
 				pcb->total_waiting_time,
